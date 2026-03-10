fe0ea650537a1c62896a9be2e0ad13053dc8a863
Julius Caesar Naag <junaag@my.cspc.edu.ph>
2026-03-10 15:32:34 +0800

    rubocop stuff

--- app/jobs/guests_cleanup_job.rb
+++ app/jobs/guests_cleanup_job.rb
@@ -5,6 +5,6 @@ class GuestsCleanupJob < ApplicationJob
 
   def perform
     Rails.logger.info "Starting GuestsCleanupJob at #{Time.current}"
-    puts 'Job is working!'
+    Rails.logger.debug 'Job is working!'
   end
 end
