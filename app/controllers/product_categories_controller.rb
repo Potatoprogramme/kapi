81f71e17bb9d9304cbbf97b5e8a7e28722ae1af1
Julius Caesar Naag <junaag@my.cspc.edu.ph>
2026-03-11 16:59:30 +0800

    In progress: create function for product categoroies

--- app/controllers/product_categories_controller.rb
+++ app/controllers/product_categories_controller.rb
@@ -4,4 +4,26 @@ class ProductCategoriesController < ApplicationController
   def index
     @categories = ProductCategory.all
   end
+
+  def new
+    @category = ProductCategory.new
+  end
+
+  def create
+    @category = ProductCategory.new(product_category_params)
+    if @category.save
+      redirect_to product_categories_path, notice: t('.success')
+    else
+    end
+  end
+
+  private
+
+  def set_product_category
+    @category = ProductCategorhy.find(params[:id])
+  end
+
+  def product_category_params
+    params.expect(category: %i[category_name description color])
+  end
 end
