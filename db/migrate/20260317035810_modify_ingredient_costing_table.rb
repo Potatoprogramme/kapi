9182c2ad608db50241d371dbe8964701f205ff46
Julius Caesar Naag <junaag@my.cspc.edu.ph>
2026-03-17 15:12:55 +0800

    migrate

--- db/migrate/20260317035810_modify_ingredient_costing_table.rb
+++ db/migrate/20260317035810_modify_ingredient_costing_table.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class ModifyIngredientCostingTable < ActiveRecord::Migration[8.1]
   def change
     rename_column :ingredient_costings, :ingredients_id, :ingredient_id
