7257cafd83a52f252184b5151a589d6cd34ee0fa
Julius Caesar Naag <junaag@my.cspc.edu.ph>
2026-03-17 14:32:18 +0800

    Working Create, Delete, Update for products. Missing: Product Costing, and Product Display

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -23,43 +23,19 @@ class ProductsController < ApplicationController
     redirect_to products_path, notice: t('.success')
   rescue StandardError => e
     load_form_data
-    @product.errors.add(:ingredients, 'cannot be empty') unless params[:product].key?(:ingredients)
+    @product.errors.add(:ingredients, 'cannot be empty') unless params[:product].key?('ingredients')
     flash.now[:alert] = e.message
     render :new, status: :unprocessable_content
-  end
-
-  def insert_product
-    @product = Product.new(product_params)
-    ActiveRecord::Base.transaction do
-      @product.save!
-      insert_ingredient('create')
-    end
-  end
-
-  def insert_ingredient(method = nil)
-    if method == 'update'
-      Ingredient.where(product_id: @product.id).destroy
-    elsif method == 'create'
-      ingredient_params.each do |material|
-        Ingredient.create!(product_id: @product.id, material_id: material['id'])
-      end
-    end
   end
 
   def update
     update_product
     redirect_to products_path, notice: t('.success')
   rescue StandardError => e
-    @product.errors.add(:ingredients, 'cannot be empty')
-    flash.now[:alert] = e.message
     load_form_data(@product.id)
-    render json: params, status: :unprocessable_content
-  end
-
-  def update_product
-    ActiveRecord::Base.transaction do
-      @product.update(product_params)
-    end
+    @product.errors.add(:ingredients, 'cannot be empty') unless params[:product].key?('ingredients')
+    flash.now[:alert] = e.message
+    render :new, status: :unprocessable_content
   end
 
   def destroy
@@ -80,11 +56,41 @@ class ProductsController < ApplicationController
   def load_form_data(product_id = nil)
     @materials = Material.all
     @categories = ProductCategory.all
-    @ingredients = Ingredient.left_joins(:material)
-                             .where(product_id: product_id).select('ingredients.*, materials.*')
+    @ingredients = Ingredient.left_joins(:ingredient_costing).left_joins(:material)
+                             .where(product_id: product_id)
+                             .select('ingredients.*, materials.*, ingredient_costings.quantity, ingredient_costings.ingredient_total_cost')
   end
 
   def ingredient_params
-    params[:product][:ingredients]&.values || []
+    params[:product][:ingredients]&.values
+  end
+
+  def insert_product
+    @product = Product.new(product_params)
+    ActiveRecord::Base.transaction do
+      @product.save!
+      insert_ingredient('create')
+    end
+  end
+
+  def update_product
+    ActiveRecord::Base.transaction do
+      @product.update(product_params)
+      insert_ingredient('update')
+    end
+  end
+
+  def insert_ingredient(method = nil)
+    Ingredient.where(product_id: @product.id).destroy_all if method == 'update'
+    ingredient_params.each do |material|
+      new = Ingredient.create!(product_id: @product.id, material_id: material['id'])
+      insert_ingredient_costing(new.id, material['quantity'], material['costPerUnit'])
+    end
+  end
+
+  def insert_ingredient_costing(ingredient_id, quantity, cost_per_unit)
+    ingredient_total_cost = (quantity.to_f * cost_per_unit.to_f).round(3)
+    IngredientCosting.create!(ingredient_id: ingredient_id, quantity: quantity,
+                              ingredient_total_cost: ingredient_total_cost)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,104 +10,106 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_16_085205) do
+ActiveRecord::Schema[8.1].define(version: 20_260_317_035_810) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "pg_catalog.plpgsql"
+  enable_extension 'pg_catalog.plpgsql'
 
-  create_table "active_storage_attachments", force: :cascade do |t|
-    t.bigint "blob_id", null: false
-    t.datetime "created_at", null: false
-    t.string "name", null: false
-    t.bigint "record_id", null: false
-    t.string "record_type", null: false
-    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
-    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  create_table 'active_storage_attachments', force: :cascade do |t|
+    t.bigint 'blob_id', null: false
+    t.datetime 'created_at', null: false
+    t.string 'name', null: false
+    t.bigint 'record_id', null: false
+    t.string 'record_type', null: false
+    t.index ['blob_id'], name: 'index_active_storage_attachments_on_blob_id'
+    t.index %w[record_type record_id name blob_id], name: 'index_active_storage_attachments_uniqueness',
+                                                    unique: true
   end
 
-  create_table "active_storage_blobs", force: :cascade do |t|
-    t.bigint "byte_size", null: false
-    t.string "checksum"
-    t.string "content_type"
-    t.datetime "created_at", null: false
-    t.string "filename", null: false
-    t.string "key", null: false
-    t.text "metadata"
-    t.string "service_name", null: false
-    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  create_table 'active_storage_blobs', force: :cascade do |t|
+    t.bigint 'byte_size', null: false
+    t.string 'checksum'
+    t.string 'content_type'
+    t.datetime 'created_at', null: false
+    t.string 'filename', null: false
+    t.string 'key', null: false
+    t.text 'metadata'
+    t.string 'service_name', null: false
+    t.index ['key'], name: 'index_active_storage_blobs_on_key', unique: true
   end
 
-  create_table "active_storage_variant_records", force: :cascade do |t|
-    t.bigint "blob_id", null: false
-    t.string "variation_digest", null: false
-    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  create_table 'active_storage_variant_records', force: :cascade do |t|
+    t.bigint 'blob_id', null: false
+    t.string 'variation_digest', null: false
+    t.index %w[blob_id variation_digest], name: 'index_active_storage_variant_records_uniqueness', unique: true
   end
 
-  create_table "ingredient_costings", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.decimal "ingredient_total_cost"
-    t.bigint "ingredients_id", null: false
-    t.decimal "quantity"
-    t.datetime "updated_at", null: false
-    t.index ["ingredients_id"], name: "index_ingredient_costings_on_ingredients_id"
+  create_table 'ingredient_costings', force: :cascade do |t|
+    t.datetime 'created_at', null: false
+    t.bigint 'ingredient_id', null: false
+    t.decimal 'ingredient_total_cost'
+    t.decimal 'quantity'
+    t.datetime 'updated_at', null: false
+    t.index ['ingredient_id'], name: 'index_ingredient_costings_on_ingredient_id'
   end
 
-  create_table "ingredients", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.bigint "material_id", null: false
-    t.bigint "product_id", null: false
-    t.datetime "updated_at", null: false
-    t.index ["material_id"], name: "index_ingredients_on_material_id"
-    t.index ["product_id"], name: "index_ingredients_on_product_id"
+  create_table 'ingredients', force: :cascade do |t|
+    t.datetime 'created_at', null: false
+    t.bigint 'material_id', null: false
+    t.bigint 'product_id', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['material_id'], name: 'index_ingredients_on_material_id'
+    t.index ['product_id'], name: 'index_ingredients_on_product_id'
   end
 
-  create_table "materials", force: :cascade do |t|
-    t.float "cost"
-    t.virtual "cost_per_unit", type: :decimal, precision: 12, scale: 4, as: "(cost / NULLIF(quantity, (0)::double precision))", stored: true
-    t.datetime "created_at", null: false
-    t.string "name"
-    t.float "quantity"
-    t.string "unit"
-    t.datetime "updated_at", null: false
+  create_table 'materials', force: :cascade do |t|
+    t.float 'cost'
+    t.virtual 'cost_per_unit', type: :decimal, precision: 12, scale: 4,
+                               as: '(cost / NULLIF(quantity, (0)::double precision))', stored: true
+    t.datetime 'created_at', null: false
+    t.string 'name'
+    t.float 'quantity'
+    t.string 'unit'
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "product_categories", force: :cascade do |t|
-    t.string "color"
-    t.datetime "created_at", null: false
-    t.string "description"
-    t.string "name"
-    t.datetime "updated_at", null: false
+  create_table 'product_categories', force: :cascade do |t|
+    t.string 'color'
+    t.datetime 'created_at', null: false
+    t.string 'description'
+    t.string 'name'
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "products", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.string "name"
-    t.bigint "product_category_id"
-    t.datetime "updated_at", null: false
-    t.index ["product_category_id"], name: "index_products_on_product_category_id"
+  create_table 'products', force: :cascade do |t|
+    t.datetime 'created_at', null: false
+    t.string 'name'
+    t.bigint 'product_category_id'
+    t.datetime 'updated_at', null: false
+    t.index ['product_category_id'], name: 'index_products_on_product_category_id'
   end
 
-  create_table "sessions", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.string "ip_address"
-    t.datetime "updated_at", null: false
-    t.string "user_agent"
-    t.bigint "user_id", null: false
-    t.index ["user_id"], name: "index_sessions_on_user_id"
+  create_table 'sessions', force: :cascade do |t|
+    t.datetime 'created_at', null: false
+    t.string 'ip_address'
+    t.datetime 'updated_at', null: false
+    t.string 'user_agent'
+    t.bigint 'user_id', null: false
+    t.index ['user_id'], name: 'index_sessions_on_user_id'
   end
 
-  create_table "users", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.string "email_address", null: false
-    t.string "password_digest", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email_address"], name: "index_users_on_email_address", unique: true
+  create_table 'users', force: :cascade do |t|
+    t.datetime 'created_at', null: false
+    t.string 'email_address', null: false
+    t.string 'password_digest', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['email_address'], name: 'index_users_on_email_address', unique: true
   end
 
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "ingredient_costings", "ingredients", column: "ingredients_id"
-  add_foreign_key "ingredients", "materials"
-  add_foreign_key "ingredients", "products"
-  add_foreign_key "products", "product_categories", on_delete: :nullify
-  add_foreign_key "sessions", "users"
+  add_foreign_key 'active_storage_attachments', 'active_storage_blobs', column: 'blob_id'
+  add_foreign_key 'active_storage_variant_records', 'active_storage_blobs', column: 'blob_id'
+  add_foreign_key 'ingredient_costings', 'ingredients'
+  add_foreign_key 'ingredients', 'materials'
+  add_foreign_key 'ingredients', 'products'
+  add_foreign_key 'products', 'product_categories', on_delete: :nullify
+  add_foreign_key 'sessions', 'users'
 end
