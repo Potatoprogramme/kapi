afc44cb9aca0d52d8e65d93300528d852680d0d1
Julius Caesar Naag <junaag@my.cspc.edu.ph>
2026-04-07 09:42:49 +0800

    Added logout route

--- config/routes/kapi_api_routes.rb
+++ config/routes/kapi_api_routes.rb
@@ -4,6 +4,7 @@ namespace :api, default: { format: :json } do
   namespace :kapi do
     namespace :v1 do
       post 'auth/login', to: 'authentication#login'
+      post 'auth/logout', to: 'authentication#logout'
       post 'register/user', to: 'registration#create'
       resources :materials, only: %i[index show create update destroy]
     end
