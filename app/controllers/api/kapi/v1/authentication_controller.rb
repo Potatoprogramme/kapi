b10a316eaa312cce72d2bbf99ce9def39fc60513
Julius Caesar Naag <junaag@my.cspc.edu.ph>
2026-04-07 13:42:42 +0800

    Refactor authentication

--- app/controllers/api/kapi/v1/authentication_controller.rb
+++ app/controllers/api/kapi/v1/authentication_controller.rb
@@ -3,11 +3,19 @@
 module Api::Kapi::V1
   class AuthenticationController < Api::Kapi::V1::ApiController
     def login
-      validate_login
+      if valid_login?
+        render :token, status: :ok
+      else
+        render_error(status: :unauthorized, message: 'Invalid Email or Password')
+      end
     end
 
     def logout
-      logout_user
+      if logout?
+        render json: { message: 'Logged out successfully' }, status: :ok
+      else
+        render_error(status: :unauthorized, message: 'Invalid refresh token')
+      end
     end
 
     def refresh
--- app/controllers/concerns/api/authentication.rb
+++ app/controllers/concerns/api/authentication.rb
@@ -1,41 +1,37 @@
 # frozen_string_literal: true
 
-module Api::Authentication
-  extend ActiveSupport::Concern
+module Api
+  module Authentication
+    extend ActiveSupport::Concern
 
-  def validate_login
-    user = User.find_by(email_address: user_params[:email_address])
-    if user&.authenticate(user_params[:password])
+    def valid_login?
+      user = User.find_by(email_address: user_params[:email_address])
+      return false unless user&.authenticate(user_params[:password])
+
       @tokens = generate_tokens(user)
       refresh_token_cookie(@tokens[:refresh_token])
-      render :token, status: :ok
-    else
-      render_error(status: :unauthorized, message: 'Invalid Email or Password')
     end
-  end
 
-  def logout_user
-    token = request.headers['Authorization']&.split&.last
-    if token
+    def logout?
+      token = request.headers['Authorization']&.split&.last
+      return false unless token
+
       cookies.delete(:refresh_token, httponly: true, secure: Rails.env.production?, same_site: :lax)
-      render json: { message: 'Logged out successfully' }, status: :ok
-    else
-      render_error(status: :bad_request, message: 'No token provided')
     end
-  end
 
-  private
+    private
 
-  def refresh_token_cookie(refresh_token)
-    cookies[:refresh_token] = {
-      value: refresh_token,
-      httponly: true,
-      secure: Rails.env.production?,
-      expires: 7.days.from_now
-    }
-  end
+    def refresh_token_cookie(refresh_token)
+      cookies[:refresh_token] = {
+        value: refresh_token,
+        httponly: true,
+        secure: Rails.env.production?,
+        expires: 7.days.from_now
+      }
+    end
 
-  def user_params
-    params.expect(user: %i[email_address password])
+    def user_params
+      params.expect(user: %i[email_address password])
+    end
   end
 end
