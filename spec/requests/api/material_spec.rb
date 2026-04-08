b1fbedfac5a827fd57080c5dae595a6cd9c63254
Julius Caesar Naag <junaag@my.cspc.edu.ph>
2026-04-08 09:57:12 +0800

    completed: update material rspec

--- spec/requests/api/material_spec.rb
+++ spec/requests/api/material_spec.rb
@@ -80,6 +80,21 @@ RSpec.describe 'Api::Kapi::V1::Materials', type: :request do
           expect(response.parsed_body['data']['name']).to eq('New material name')
         end
       end
+      context 'with invalid material parameters' do
+        it 'returns unprocessable content and an error message' do
+          update_attributes = attributes_for(:material).merge(name: nil)
+
+          patch "#{api_base}/#{material.id}", headers: { 'Authorization' => "Bearer #{@access_token}" },
+                                              params: { material: update_attributes }, as: :json
+          expect(response).to have_http_status(:unprocessable_content)
+          expect(response.parsed_body['error']).to be_present
+          expect(response.parsed_body['error']['message']).to be_present
+          expect(response.parsed_body['error']['errors']).to be_present
+        end
+      end
     end
+    it_behaves_like 'unauthorized user', lambda {
+      patch "#{api_base}/#{material.id}", as: :json
+    }
   end
 end
