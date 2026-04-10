bb015cb816d5cf1a97281148f85d7b4a76881fa0
Julius Caesar Naag <junaag@my.cspc.edu.ph>
2026-04-10 11:27:09 +0800

    rename product_categories_spec.rb

---
+++ spec/requests/api/product_categories_spec.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+RSpec.shared_examples 'unauthorized user' do |request_call|
+  context 'when user is not authenticated' do
+    it 'returns unauthorized' do
+      instance_exec(&request_call)
+      expect(response).to have_http_status(:unauthorized)
+    end
+  end
+end
+
+Rspec.shared_context 'authenticated request' do
+  let(:user) { create(:user) }
+  let(:auth_headers) do
+    post '/api/kapi/v1/login', params: { user: {
+      email_address: user.email_address,
+      password: user.password
+    } }, as: :json
+
+    expect(response).to have_http_status(:ok)
+    expect(response.parsed_body['access_token']).to be_present
+    token = response.parsed_body['access_token']
+
+    { 'Authoriazation' => "Bearer #{token}" }
+  end
+end
+RSpec.describe 'Api::Kapi::V1::ProductCategories', type: :request do
+  let(:product_category) { create(:product_category) }
+  describe 'GET api/kapi/v1/product_categories' do
+    it 'returns a list of product categories' do
+      get '/api/kapi/v1/product_categories', as: :json
+      expect
+    end
+  end
+end
--- spec/requests/api/product_categories_sped.rb
+++
@@ -1,4 +0,0 @@
-# frozen_string_literal: true
-
-RSpec.describe 'Api::Kapi::V1::ProductCategories', type: :request do
-end
