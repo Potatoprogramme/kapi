2682f0ffd8390834c5ed3c6cefc53b338c9493df
Julius Caesar Naag <junaag@my.cspc.edu.ph>
2026-04-15 13:32:02 +0800

    added a helper so I can quickly deliver a message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,8 @@ class ApplicationController < ActionController::Base
 
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
+
+  def alert(message)
+    flash.now[:alert] = message
+  end
 end
