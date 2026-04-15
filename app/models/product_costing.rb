000cef251c0f804150cb962973bf3fb373b3781c
Julius Caesar Naag <junaag@my.cspc.edu.ph>
2026-04-15 09:39:26 +0800

    Updated validation for costing

--- app/models/product_costing.rb
+++ app/models/product_costing.rb
@@ -7,14 +7,24 @@ class ProductCosting < ApplicationRecord
   validate :costing_values_are_consistent
 
   def costing_values_are_consistent
-    overhead_cost = (direct_cost * (overhead_percentage / 100)).to_f.round(2)
-    total_cost = (overhead_cost + direct_cost).to_f.round(2)
-    recommended_selling = (total_cost / (1 - (profit_margin_percentage / 100))).to_f.round(2)
-    profit_margin_amount = (recommended_selling - total_cost).to_f.round(2)
-    unless overhead_cost == self.overhead_cost &&
-           total_cost == self.total_cost &&
-           profit_margin_amount == self.profit_margin_amount
-      errors.add(:base, 'Costing values are inconsistent')
+    return if [direct_cost, overhead_percentage, profit_margin_percentage, overhead_cost, total_cost,
+               profit_margin_amount].any?(&:nil?)
+
+    # Avoid division by zero / negative denominator
+    if profit_margin_percentage.to_d >= 100
+      errors.add(:profit_margin_percentage, 'must be less than 100')
+      return
     end
+
+    expected_overhead_cost = (direct_cost.to_d * (overhead_percentage.to_d / 100)).round(2)
+    expected_total_cost = (direct_cost.to_d + expected_overhead_cost).round(2)
+    expected_recommended_selling = (expected_total_cost / (1 - (profit_margin_percentage.to_d / 100))).round(2)
+    expected_profit_margin_amount = (expected_recommended_selling - expected_total_cost).round(2)
+
+    return if overhead_cost.to_d.round(2) == expected_overhead_cost &&
+              total_cost.to_d.round(2) == expected_total_cost &&
+              profit_margin_amount.to_d.round(2) == expected_profit_margin_amount
+
+    errors.add(:base, 'Costing values are inconsistent')
   end
 end
