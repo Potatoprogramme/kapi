024b0ae881c767a8c35723c11413cc17355ea524
Julius Caesar Naag <junaag@my.cspc.edu.ph>
2026-04-18 14:43:55 +0800

    Added: payment_method to validation

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,7 +2,7 @@
 
 class Order < ApplicationRecord
   belongs_to :user
-  validates :order_total, presence: true
+  validates :order_total, :payment_method, presence: true
   has_many :order_items, dependent: :destroy
 
   enum :payment_method, { cash: 0, gcash: 1, card: 2, maya: 3 }
