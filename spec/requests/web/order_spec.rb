93a0b340fb6d648031c1de27002901853206bc17
Julius Caesar Naag <junaag@my.cspc.edu.ph>
2026-04-22 14:21:20 +0800

    push

--- spec/requests/web/order_spec.rb
+++ spec/requests/web/order_spec.rb
@@ -1,7 +1,16 @@
-require 'rails_helper'
+# frozen_string_literal: true
 
+require 'rails_helper'
+require_relative 'support/shared_context/web_authentication'
 RSpec.describe 'Orders', type: :request do
+  include_context 'web authenticated request'
+  let!(:order) { create(:order, user: user) }
+
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+    it 'assigns all orders to @orders and renders orders index' do
+      get orders_path
+      expect(response.body).to include(order.name)
+      expect(response).to have_http_status(:ok)
+    end
   end
 end
